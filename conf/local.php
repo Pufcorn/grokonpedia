d5ebb81975065b0d0bf79b3c9610cb08228ae3d5
Pufcorn <pufcorn@riseup.net>
2025-11-01 10:12:27 -0400

    Create local.php

---
+++ conf/local.php
@@ -0,0 +1,8 @@
+<?php
+$conf['title'] = 'Grokonpedia';
+$conf['lang'] = 'fr';
+$conf['basedir'] = '';
+$conf['useacl'] = 1;
+$conf['superuser'] = '@admin';
+$conf['disableactions'] = '';
+$conf['userewrite'] = 'o';
