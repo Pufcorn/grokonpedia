f7fed1ff2e5721bffb90ff04c0d673404feff378
Pufcorn <pufcorn@riseup.net>
2026-04-15 15:09:51 -0400

    Delete conf/local.php

--- conf/local.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conf['title'] = 'Grokonpedia';
-$conf['lang'] = 'fr';
-$conf['basedir'] = '';
-$conf['useacl'] = 1;
-$conf['superuser'] = '@admin';
-$conf['disableactions'] = '';
-$conf['userewrite'] = 'o';
